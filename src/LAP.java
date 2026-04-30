7835c91180e6695cd8f05189769d13cb30efc9e6
Stephen Siegel <siegel@giacomo.local>
2026-04-30 08:14:41 -0400

    Added the missing main file LAP.java. Cleaned up FOL grammar.

---
+++ src/LAP.java
@@ -0,0 +1,14 @@
+import org.l4cs.cli.Executor;
+
+/**
+ * LAP: Logic, Algorithms, Proof tool. Command line interface.
+ *
+ * @author Stephen Siegel
+ * @author Yuxin Zhou
+ */
+public class LAP {
+
+	public final static void main(String[] args) {
+		Executor.execute(args);
+	}
+}
